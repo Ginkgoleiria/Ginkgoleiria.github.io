b971bdb21ba6d760bb7b4e23538fbcaeb240de8d
chushi0 <wangyongchao49@sina.com>
2021-02-18 18:54:59 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,71 @@
+const DATA = [
+    {
+        type: "热销",
+        content: [
+            {
+                img: "img/rice.jpg",
+                name: "米饭",
+                detail: "香米，捞饭已含米饭",
+                price: "2.88"
+            }, {
+                img: "img/lmjt.jpg",
+                name: "老母鸡汤",
+                detail: "含米饭",
+                price: "39.8"
+            }, {
+                img: "img/yzdxy.jpg",
+                name: "椰汁炖雪燕",
+                detail: "开业收藏",
+                price: "13.8"
+            }, {
+                img: "img/hhtc.jpg",
+                name: "豪华2-3人套餐",
+                price: "128"
+            }
+        ]
+    }, {
+        type: "折扣",
+        content: [
+            {
+                img: "img/bzlf.jpg",
+                name: "传承鲍汁捞饭",
+                detail: "成鲜，花胶，海参，虾仁，北极贝",
+                price: "65.8"
+            }, {
+                img: "img/yrtc.jpg",
+                name: "奢华一人套餐",
+                detail: "成鲜，虾仁，木耳，鲍鱼",
+                price: "88"
+            }, {
+                img: "img/xj.jpg",
+                name: "茶楼虾饺皇*五个装*",
+                detail: "原味，虾",
+                price: "16.8"
+            }
+        ]
+    }, {
+        type: "招牌",
+        content: [
+            {
+                img: "img/ftq.jpg",
+                name: "帝王佛跳墙",
+                detail: "昔日帝王宴如今桌上餐~",
+                price: "151.2"
+            }, {
+                img: "img/bymxr.jpg",
+                name: "鲍鱼闷虾仁",
+                detail: "原味，鲍鱼，虾仁",
+                price: "53.8"
+            }
+        ]
+    }, {
+        type: "热饮",
+        content: [
+            {
+                img: "img/nc.jpg",
+                name: "原味奶茶",
+                price: "30"
+            }
+        ]
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+    let left = $(".left-content");
+    let right = $(".right-content");
+    let left_titles = [];
+    let right_titles = [];
+    for (let data of DATA) {
+        let title = $("<li/>").text(data.type);
+        right.append(title);
+        for (let content of data.content) {
+            let item = $("<div/>").addClass("item");
+            let img = $("<img/>").attr({
+                src: content.img
+            });
+            let name = $("<span/>").addClass("title").text(content.name);
+            let detail = $("<span/>").addClass("detail of").text(content.detail);
+            let price = $("<span/>").addClass("price").text(content.price);
+            right.append($("<li/>").append(item.append(img, $("<div/>").addClass("info").append(name, detail, price))));
+        }
+        let lt = $("<li/>").text(data.type);
+        lt.click(function () {
+            right[0].scrollTop = title[0].offsetTop;
+            setTimeout(() => {
+                for(let e of left_titles) {
+                    e.removeClass("active");
+                }
+                lt.addClass("active");
+            }, 10);
+        });
+        left.append(lt);
+
+        left_titles.push(lt);
+        right_titles.push(title);
+    }
+
+    right.scroll(function() {
+        let st = right[0].scrollTop;
+        for(let e of left_titles) {
+            e.removeClass("active");
+        }
+        for(let i in left_titles) {
+            if(right_titles[i][0].offsetTop > st) {
+                left_titles[i - 1].addClass("active");
+                return;
+            }
+        }
+        left_titles[left_titles.length - 1].addClass("active");
+    })
+
+    left_titles[0].addClass("active");
+})
